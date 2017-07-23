e31bbf6bc06e9ae65f91126c489bcb97e2561ab1
Wellington Guimaraes <wellguimaraes@gmail.com>
2017-07-23 11:18:43 -0300

    initial commit

---
+++ choco.bat
@@ -0,0 +1,15 @@
+Set-ExecutionPolicy AllSigned
+iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+choco install googlechrome -y
+choco install nodejs -y
+choco install git -y
+choco install webstorm -y
+choco install telegram -y
+choco install winrar -y
+choco install sublimetext3 -y
+choco install docker -y
+choco install mongodb -y
+choco install microsoft-build-tools -y
+choco install firacode -y
+choco install autohotkey -y
+choco install jdk8 -y

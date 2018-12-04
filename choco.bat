c67e0c53965bbb4393bea60e393c4ce3ac7aadec
Wellington Guimaraes <wellguimaraes@gmail.com>
2018-12-04 14:20:27 -0300

    Update choco.bat

--- choco.bat
+++ choco.bat
@@ -1,5 +1,5 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-choco install firacode telegram googlechrome git microsoft-build-tools nvm webstorm winrar sublimetext3 jdk8 firefox skype vlc slack autohotkey spotify docker -y
+choco install telegram googlechrome git microsoft-build-tools nvm webstorm winrar wordweb-free sublimetext3 jdk8 firefox skype vlc slack autohotkey spotify docker -y
 nvm install 11.3.0
 nvm use 11.3.0
 npm i -g yarn cross-env concurrently nodemon create-react-app typescript flow-bin

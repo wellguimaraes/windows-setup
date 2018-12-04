52011226a776683d18098f4941a0aa97fa39f645
Wellington Guimaraes <wellguimaraes@gmail.com>
2018-12-04 09:27:45 -0300

    Update choco.bat

--- choco.bat
+++ choco.bat
@@ -1,18 +1,5 @@
-Set-ExecutionPolicy AllSigned
-iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-choco install googlechrome -y
-choco install nodejs -y
-choco install git -y
-choco install webstorm -y
-choco install telegram -y
-choco install winrar -y
-choco install sublimetext3 -y
-choco install docker -y
-choco install mongodb -y
-choco install microsoft-build-tools -y
-choco install autohotkey -y
-choco install jdk8 -y
-choco install firefox -y
-choco install skype -y
-choco install vlc -y
-choco install slack -y
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+choco install firacode telegram googlechrome git microsoft-build-tools nvm webstorm winrar sublimetext3 jdk8 firefox skype vlc slack autohotkey spotify docker -y
+nvm install 11.3.0
+nvm use 11.3.0
+npm i -g yarn cross-env concurrently nodemon create-react-app typescript flow-bin
